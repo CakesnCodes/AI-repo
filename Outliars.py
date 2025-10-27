51e8160d02ae41938427cff24c3e2b74496a27bc
CakesnCodes <scribblechronicles@protonmail.com>
2025-10-27 20:08:27 +0400

    Update Outliars.py

--- Outliars.py
+++ Outliars.py
@@ -38,18 +38,4 @@ def casndra():
 new_col = pd.DataFrame({"Predicted Quality":casndra()})
 df = pd.concat([df, new_col], axis=1)
 
-res2 = df['Predicted Quality']
-
-"""accuracy_1 = accuracy_score(df['Quality'], df['Predicted Quality'])
-precision_1 = precision_score(df['Quality'], df['Predicted Quality'], pos_label="good")
-recall_1 = recall_score(df['Quality'], df['Predicted Quality'], pos_label="good")
-f1_1 = f1_score(df['Quality'], df['Predicted Quality'], pos_label="good")
-
-print('Evaluation:')
-print(f'Accuracy: {accuracy_1:.4f}')
-print(f'Precision: {precision_1:.4f}')
-print(f'Recall: {recall_1:.4f}')
-print(f'F1 Score: {f1_1:.4f}')
-"""
-
     
