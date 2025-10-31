2e2adac50aee8d12bce391543a0891790e2e7aec
CakesnCodes <scribblechronicles@protonmail.com>
2025-10-31 21:08:37 +0400

    Update Eval plots.py

--- Eval plots.py
+++ Eval plots.py
@@ -5,7 +5,7 @@ scores = ['Accuracy', 'Precision', 'Recall', 'F1']
 c1 = [0.5045, 0.5440, 0.8014, 0.6481]
 c2 = [0.5353, 0.5028, 0.9985, 0.6688]
 
-plt.figure()
+plt.figure() #for a bar chart that shows evaluation metrics for both Honeybee and Fireblight
 w, x = 0.4, np.arange(len(scores))
 plt.bar(x - w/2, c1, w, label='Honeybee', color='yellow')
 plt.bar(x + w/2, c2, w, label='Fireblight', color='red')
@@ -16,8 +16,7 @@ plt.title('Evaluation metrics of Honeybee and Fireblight')
 
 plt.show()
 
-
-plt.figure()
+plt.figure() #Honeybee's evaluation metrics
 plt.bar(scores, c1, color='yellow')
 plt.xticks(scores)
 plt.ylabel('Score')
@@ -25,7 +24,7 @@ plt.title('Evaluation metrics of Honeybee')
 
 plt.show()
 
-plt.figure()
+plt.figure() #Fireblight's evaluation metrics
 plt.bar(scores, c2, color='red')
 plt.xticks(scores)
 plt.ylabel('Score')
@@ -33,3 +32,4 @@ plt.title('Evaluation metrics of Fireblight')
 
 
 plt.show()
+
